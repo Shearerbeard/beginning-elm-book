4f24f93e949e7b4078fbbb93e76c93bdb146f258
Mike Shearer <shearerbeard@gmail.com>
2018-01-05 19:12:59 -0700

    adds data structures from types chapter

--- src/Playground.elm
+++ src/Playground.elm
@@ -1,5 +1,7 @@
 module Playground exposing (..)
 
+import Regex
+
 
 multiplier : Int
 multiplier =
@@ -61,6 +63,115 @@ welcomeMessage isLoggedIn =
 
         False ->
             "Please log in"
+
+
+signUp : String -> String -> Result String String
+signUp email age =
+    case String.toInt age of
+        Err message ->
+            Err message
+
+        Ok age ->
+            let
+                emailPattern =
+                    Regex.regex "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b"
+
+                isValidEmail =
+                    Regex.contains emailPattern email
+            in
+                if age < 13 then
+                    Err "You need to be at least 13 years old to sign up."
+                else if isValidEmail then
+                    Ok "Your account has been created successfully!"
+                else
+                    Err "You entered an invalid email."
+
+
+type alias Character =
+    { name : String
+    , age : Maybe Int
+    }
+
+
+sansa : Character
+sansa =
+    { name = "Sansa"
+    , age = Just 19
+    }
+
+
+arya : Character
+arya =
+    { name = "Arya"
+    , age = Nothing
+    }
+
+
+jonSnow : Character
+jonSnow =
+    Character "Jon Snow" <| Just 21
+
+
+rickon : Character
+rickon =
+    Character "Rickon" <| Just 11
+
+
+robb : Character
+robb =
+    Character "Robb" <| Just 18
+
+
+getAdultAge : Character -> Maybe Int
+getAdultAge character =
+    case character.age of
+        Nothing ->
+            Nothing
+
+        Just age ->
+            if age >= 18 then
+                Just age
+            else
+                Nothing
+
+
+type MyList a
+    = EmptyList
+    | ListNode a (MyList a)
+
+
+sum : MyList Int -> Int
+sum myList =
+    case myList of
+        EmptyList ->
+            0
+
+        ListNode intValue remainingValues ->
+            intValue + sum remainingValues
+
+
+type Tree a
+    = Empty
+    | Node a (Tree a) (Tree a)
+
+
+exampleTree : Tree Int
+exampleTree =
+    Node 1
+        (Node 2
+            (Node 4
+                Empty
+                (Node 8 Empty Empty)
+            )
+            (Node 5 Empty Empty)
+        )
+        (Node 3
+            (Node 6 Empty Empty)
+            (Node 7
+                (Node 9 Empty Empty)
+                Empty
+            )
+        )
 
 
 
